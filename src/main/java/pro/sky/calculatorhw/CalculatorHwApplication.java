37897d43b1f51c883178246b02a1b7289fc4c666
KhonSS <xonchik_95@mail.ru>
2022-08-21 18:21:37 +0900

    первый коммит

---
+++ src/main/java/pro/sky/calculatorhw/CalculatorHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorHwApplication.class, args);
+	}
+
+}
